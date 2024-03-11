cd9995530611c8d78c08ba912ba010f49fabf71c
Maciej Oliwa <maraus92@gmail.com>
2024-03-11 15:52:16 +0100

    Seg fault example

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
+#include <stdio.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    // Explicit segfault to test the debugger
+    int a = 123;
+    int* p = nullptr;
+
+    printf("Wartosc (int) A -->   %d\n", a);
+    printf("Wskaznik A na int --> %p\n", &a);
+
+    printf("Wartosc P (wskaznika na int) --> %d\n", p);
+    printf("Wskaznik P na int -->            %p\n", &p);
+    *p = 420;
+    std::cout << "This line will not be printed" << std::endl;
+
     return 0;
 }
