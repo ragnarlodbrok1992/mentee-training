d01ad56435f577f4efef16ae5b20b0b59b34f548
Agnieszka <agne6ka@gmail.com>
2024-04-25 21:32:10 +0200

    Add a DVD screensaver that changes colors.

--- SDL2Game/main.cpp
+++ SDL2Game/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <random>
 
-const int WIDTH = 800, HEIGHT = 600;
+const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
 
 // TODO: exercise 1) dvd screensaver, bouncing rect & change colors
 // TODO: exercise 2) dvd screensaver, add texture to rect
+int randomize(int maxRange)
+{
+    std::random_device rd;                              // obtain a random number from hardware
+    std::mt19937 gen(rd());                             // seed the generator
+    std::uniform_int_distribution<> distr(0, maxRange); // define the range
+    return distr(gen);                                  // generate numbers
+};
+void coordinates(SDL_Rect shape)
+{
+    printf("X:%d Y:%d\n", shape.x, shape.y);
+};
 
 int main(int arg, char *argv[])
 {
@@ -12,9 +24,9 @@ int main(int arg, char *argv[])
 
     SDL_Window *window = SDL_CreateWindow(
         "SDL Window",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        WIDTH, HEIGHT,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
         SDL_WINDOW_OPENGL);
 
     if (NULL == window)
@@ -23,9 +35,7 @@ int main(int arg, char *argv[])
         return 1;
     }
 
-    SDL_Event event;
-    SDL_KeyCode key;
-    SDL_Renderer *gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED && SDL_RENDERER_PRESENTVSYNC);
 
     if (NULL == gRenderer)
     {
@@ -35,17 +45,28 @@ int main(int arg, char *argv[])
 
     bool running = true;
 
-    // x, y top left corner
-    SDL_Rect rect =
+    int rectWidth = 50;
+    int rectHeight = 50;
+
+    // Set rect to random initial position
+    SDL_Rect srcRect =
         {
-            100,
-            100,
-            100,
-            100,
+            randomize(SCREEN_WIDTH - rectWidth),   // x from left to right
+            randomize(SCREEN_HEIGHT - rectHeight), // y from top to bottom
+            rectWidth,                             // width
+            rectHeight,                            // height
         };
+
+    int moveX = 1;
+    int moveY = 1;
+    int fillRed = 0;
+    int fillGreen = 0;
+    int fillBlue = 255;
 
     while (running) // One cycle frame
     {
+
+        SDL_Event event;
         // Check the close event and keypress
         if (SDL_PollEvent(&event))
         {
@@ -58,27 +79,8 @@ int main(int arg, char *argv[])
                 // TODO: Remember to optimize arrow motion keypresses
                 switch (event.key.keysym.sym)
                 {
-                case SDLK_DOWN:
-                    std::cout << "Go down" << std::endl;
-                    rect.y += 10;
-                    break;
-                case SDLK_UP:
-                    std::cout << "Go up" << std::endl;
-                    rect.y -= 10;
-                    break;
-                case SDLK_RIGHT:
-                    std::cout << "Go right" << std::endl;
-                    rect.x += 10;
-                    break;
-                case SDLK_LEFT:
-                    std::cout << "Go left" << std::endl;
-                    rect.x -= 10;
-                    break;
                 case SDLK_ESCAPE:
                     running = false;
-                    break;
-                default:
-                    std::cout << "Key was pressed: " << SDL_GetKeyName(event.key.keysym.sym) << std::endl;
                     break;
                 }
             }
@@ -89,9 +91,32 @@ int main(int arg, char *argv[])
         SDL_RenderClear(gRenderer);
 
         // Draw red rect
-        SDL_Rect fillRect = {rect};
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-        SDL_RenderFillRect(gRenderer, &fillRect);
+        SDL_SetRenderDrawColor(gRenderer, fillRed, fillGreen, fillBlue, 0xFF);
+        SDL_RenderFillRect(gRenderer, &srcRect);
+
+        // TODO: Learn more about Motion (integration methods, interpolation, etc.)
+
+        if (srcRect.x == 0 || srcRect.x == SCREEN_WIDTH - rectWidth)
+        {
+            moveX = -moveX;
+            fillRed = randomize(255);
+            fillGreen = randomize(255);
+            fillBlue = randomize(255);
+        };
+        if (srcRect.y == 0 || srcRect.y == SCREEN_HEIGHT - rectHeight)
+        {
+            moveY = -moveY;
+            fillRed = randomize(255);
+            fillGreen = randomize(255);
+            fillBlue = randomize(255);
+        };
+
+        srcRect.x += moveX;
+        srcRect.y += moveY;
+
+        coordinates(srcRect);
+
+        SDL_Delay(50);
 
         // Draw to the screen
         SDL_RenderPresent(gRenderer);
