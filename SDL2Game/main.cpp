7125bf8e0e429c064d2c10dd323df621d9ca7ccb
Agnieszka <agne6ka@gmail.com>
2024-05-03 12:15:49 +0200

    Move animation to class, create constants.

--- SDL2Game/main.cpp
+++ SDL2Game/main.cpp
@@ -3,9 +3,13 @@
 #include "CRnd.h"
 #include "CTexture.h"
 #include "CCircle.h"
+#include "CAnimate.h"
+#include "Constant.h"
 
 SDL_Window *window;
 SDL_Renderer *gRenderer;
+
+using namespace Constant;
 
 /**
  * @brief TODO:
@@ -13,8 +17,6 @@ SDL_Renderer *gRenderer;
  * - [] Add a counter that will display numbers at the top of the screen.
  * - [] Handle the collision and update the counter.
  */
-
-const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
 
 void coordinates(SDL_Rect shape)
 {
@@ -51,7 +53,7 @@ int main(int arg, char *argv[])
     const int rectWidth = 50;
     const int rectHeight = 50;
 
-    CRnd posX, posY, color;
+    CRnd posX, posY;
     CTexture DvDLogo(gRenderer, "./src/images/DVD-Logo-w.bmp");
 
     // Set rect to random initial position
@@ -63,8 +65,8 @@ int main(int arg, char *argv[])
             rectHeight,                                    // height
         };
 
-    int moveX = 2;
-    int moveY = 2;
+    CAnimate rectangle(&srcRect, 2, 2);
+
     Uint8 fillRed = 0;
     Uint8 fillGreen = 0;
     Uint8 fillBlue = 255;
@@ -100,27 +102,8 @@ int main(int arg, char *argv[])
         DvDLogo.Render(gRenderer, &srcRect);
         DvDLogo.SetColor(fillRed, fillGreen, fillBlue);
 
-        // TODO: Learn more about Motion (integration methods, interpolation, etc.)
-
-        if (srcRect.x <= 0 + abs(moveX) || srcRect.x >= SCREEN_WIDTH - rectWidth - abs(moveX))
-        {
-            moveX = -moveX;
-            fillRed = color.getRandom();
-            fillGreen = color.getRandom();
-            fillBlue = color.getRandom();
-        };
-        if (srcRect.y <= 0 + abs(moveY) || srcRect.y >= SCREEN_HEIGHT - rectHeight - abs(moveY))
-        {
-            moveY = -moveY;
-            fillRed = color.getRandom();
-            fillGreen = color.getRandom();
-            fillBlue = color.getRandom();
-        };
-
-        srcRect.x += moveX;
-        srcRect.y += moveY;
-
-        coordinates(srcRect);
+        rectangle.Start();
+        // coordinates(srcRect);
 
         CCircle Point1(gRenderer, {100, 150, 25}, {255, 0, 0});
         CCircle Point2(gRenderer, {400, 450, 25}, {0, 0, 255});
---
+++ SDL2Game/src/include/Constant.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+// define your own namespace to hold constants
+namespace Constant
+{
+    constexpr double PI = 3.14159;
+    constexpr int SCREEN_WIDTH = 800;
+    constexpr int SCREEN_HEIGHT = 600;
+
+}
+#endif
