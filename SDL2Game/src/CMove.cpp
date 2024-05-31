557233cdb4e01c4873cd5288303a42cc3ae63262
Agnieszka <agne6ka@gmail.com>
2024-05-31 13:16:27 +0200

    Add CMove class description and todos.

---
+++ SDL2Game/src/CMove.cpp
@@ -0,0 +1,28 @@
+#include "CMove.h"
+
+/** // TODO: Implement entities movement
+ * @brief Allow game characters to:
+ * [] Run to move the game character forward.
+ * [] Jump over the objects and on them.
+ * [] Dash into objects to smash them.
+ */
+
+CMove::CMove()
+{
+}
+
+CMove::~CMove()
+{
+}
+
+void CMove::Run()
+{
+}
+
+void CMove::Jump()
+{
+}
+
+void CMove::Dash()
+{
+}
---
+++ SDL2Game/src/include/CMove.h
@@ -0,0 +1,18 @@
+#ifndef MOVE_CLASS_H
+#define MOVE_CLASS_H
+
+class CMove
+{
+public:
+    // Constructor
+    CMove();
+    void Run();
+    void Jump();
+    void Dash();
+    // Destructor
+    ~CMove();
+
+private:
+};
+
+#endif
